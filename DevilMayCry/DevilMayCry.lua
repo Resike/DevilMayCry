87c3f9ad4ec35e4ab98859ab332da5bdad41bc86
Resike <resike@gmail.com>
2013-08-25 20:45:46 +0200

    1.0.0 Alpha 2

--- DevilMayCry/DevilMayCry.lua
+++ DevilMayCry/DevilMayCry.lua
@@ -2,135 +2,102 @@ local _, ns = ...
 local DevilMayCry = { }
 ns.DevilMayCry = DevilMayCry
 
-local BaseSize = 128
-local TextureSize = 128
-local AnimSize = 300
-local ReachedMax = false
+local BASE_SIZE = 128
+local MAX_SIZE = BASE_SIZE * 2.4
+local UPDATE_INTERVAL = 0.005
 
-local Frame = CreateFrame("Frame", nil, UIParent)
-Frame:SetPoint("CENTER", 500, 450)
-Frame:SetFrameStrata("Medium")
-Frame:SetFrameLevel(0)
-Frame:SetWidth(TextureSize)
-Frame:SetHeight(TextureSize)
-Frame:SetAlpha(0.99)
-Frame:SetMovable(true)
-Frame:EnableMouse(true)
-Frame:SetClampedToScreen(false)
-Frame:RegisterForDrag("LeftButton")
+local SCALE_DELTA = 0.06
+local MAX_SCALE = MAX_SIZE / BASE_SIZE + SCALE_DELTA
+local WIDTH_SCALE = 1
 
-local Background = Frame:CreateTexture("Texture", "Background")
-Background:SetTexture("Interface\\Addons\\DevilMayCry\\Textures\\Background")
-Background:SetWidth(TextureSize)
-Background:SetHeight(TextureSize)
-Background:SetBlendMode("Disable")
-Background:SetDrawLayer("Background", 0)
-Background:SetAllPoints(Frame)
+local currentScale, percentCompleted = 1, 1
 
-local Foreground = Frame:CreateTexture("Texture", "Background")
-Foreground:SetTexture("Interface\\Addons\\DevilMayCry\\Textures\\Foreground")
-Foreground:SetWidth(TextureSize)
-Foreground:SetHeight(TextureSize)
-Foreground:SetBlendMode("Disable")
-Foreground:SetDrawLayer("Background", 1)
-Foreground:SetPoint("Bottom", Frame, "Bottom")
+local frame = CreateFrame("Frame", nil, UIParent)
+frame:SetPoint("Center", 500, 450)
+frame:SetFrameStrata("Medium")
+frame:SetFrameLevel(0)
+frame:SetWidth(BASE_SIZE)
+frame:SetHeight(BASE_SIZE)
+frame:SetMovable(true)
+frame:EnableMouse(true)
+frame:SetClampedToScreen(false)
+frame:RegisterForDrag("LeftButton")
 
-local UpdateFrame = CreateFrame("Frame", nil)
-local TimeSinceLastUpdate = 0
-local NextUpdate = 0.0050
+local bgTexture = frame:CreateTexture(nil, "Background")
+bgTexture:SetTexture([[Interface\Addons\DevilMayCry\Textures\DB]])
+bgTexture:SetWidth(BASE_SIZE)
+bgTexture:SetHeight(BASE_SIZE)
+bgTexture:SetAllPoints()
+bgTexture:SetDrawLayer("Background", 5)
 
-function DevilMayCry.StartMove(frame, button)
-	if button ~= "LeftButton" then
-		return
+local fgTexture = frame:CreateTexture(nil, "Background")
+fgTexture:SetTexture([[Interface\Addons\DevilMayCry\Textures\DF]])
+fgTexture:SetWidth(BASE_SIZE)
+fgTexture:SetHeight(BASE_SIZE)
+fgTexture:SetPoint("Bottom")
+fgTexture:SetDrawLayer("Background", 6)
+
+frame:SetScript("OnMouseDown", function(self, button)
+	if button == "LeftButton" then
+		self:StartMoving()
 	end
-	frame:StartMoving()
-end
+end)
 
-Frame:SetScript("OnMouseDown", DevilMayCry.StartMove)
-
-function DevilMayCry.StopMove(frame, button)
-	if button ~= "LeftButton" then
-		return
+frame:SetScript("OnMouseUp", function(self, button)
+	if button == "LeftButton" then
+		self:StopMovingOrSizing()
+		local L1, B1, W1, H1 = UIParent:GetRect()
+		local L2, B2, W2, H2 = self:GetRect()
+		self:ClearAllPoints()
+		self:SetPoint("CENTER", L2 - L1 + (W2 - W1) / 2, B2 - B1 + (H2 - H1) / 2)
 	end
-	frame:StopMovingOrSizing()
-	local x = math.floor(frame:GetLeft() + (frame:GetWidth() - UIParent:GetWidth()) / 2 + 0.5)
-	local y = math.floor(frame:GetTop() - (frame:GetHeight() + UIParent:GetHeight()) / 2 + 0.5)
-	frame:ClearAllPoints()
-	frame:SetPoint("CENTER", x, y)
-end
-
-Frame:SetScript("OnMouseUp", DevilMayCry.StopMove)
-
-function DevilMayCry:SetSize()
-	if Foreground:GetHeight() > TextureSize * 0.9999 then
-		ReachedMax = true
-	end
-	if Foreground:GetHeight() <= 0 then
-		-- Test Mode
-		Frame:SetWidth(TextureSize)
-		Frame:SetHeight(TextureSize)
-		Background:SetAllPoints(Frame)
-		Foreground:SetWidth(TextureSize)
-		Foreground:SetHeight(TextureSize)
-		Foreground:SetTexCoord(0, 0, 0, 1, 1, 0, 1, 1)
-	else
-		local y = Foreground:GetHeight() / TextureSize
-		if y > 1 then
-			Foreground:SetHeight(TextureSize)
-			y = 1
-		end
-		Frame:SetHeight(TextureSize)
-		Frame:SetWidth(TextureSize)
-		Background:SetAllPoints(Frame)
-		if Foreground:GetWidth() > TextureSize then
-			Foreground:SetWidth(Foreground:GetWidth() - ((TextureSize / 128) * 0.1))
-		end
-		Foreground:SetHeight(Foreground:GetHeight() - ((TextureSize / 128) * 0.1))
-		local ULx, ULy, LLx, LLy, URx, URy, LRx, LRy = Foreground:GetTexCoord()
-		Foreground:SetTexCoord(ULx, 1 - y, LLx, LLy, URx, 1 - y, LRx, LRy)
-	end
-	return 0.0050
-end
-
-function DevilMayCry:SetTextureSize(size)
-	local LastSize = TextureSize
-	TextureSize = size
-	Foreground:SetWidth(Foreground:GetWidth() * TextureSize / LastSize)
-	Foreground:SetHeight(Foreground:GetHeight() * TextureSize / LastSize)
-end
-
-function DevilMayCry:AnimateTexture()
-	DevilMayCry:SetTextureSize(AnimSize)
-	if AnimSize >= (BaseSize + 2) then
-		AnimSize = AnimSize - 2
-	else
-		AnimSize = 300
-		ReachedMax = false
-	end
-end
+end)
 
 function DevilMayCry_IncreaseHeight(percent)
-	if Foreground:GetHeight() < (TextureSize * (1 - percent)) then
-		Foreground:SetHeight(Foreground:GetHeight() + (TextureSize * percent))
+	if fgTexture:GetHeight() < (BASE_SIZE * (1 - percent)) then
+		percentCompleted = percentCompleted + percent
 	else
-		Foreground:SetHeight(TextureSize)
+		percentCompleted = 1
+		currentScale = MAX_SCALE
 	end
-	if Foreground:GetWidth() <= (TextureSize) then
-		Foreground:SetWidth(Foreground:GetWidth() * 1.12)
-	else
-		Foreground:SetWidth(TextureSize * 1.12)
-	end
+	WIDTH_SCALE = 1.12
 end
 
-function DevilMayCry:OnUpdate(elapsed)
-	TimeSinceLastUpdate = TimeSinceLastUpdate + elapsed
-	while TimeSinceLastUpdate > NextUpdate do
-		TimeSinceLastUpdate = TimeSinceLastUpdate - NextUpdate
-		if ReachedMax == true then
-			DevilMayCry:AnimateTexture()
+local SetSize, SetTexCoord = frame.SetSize, fgTexture.SetTexCoord
+
+do
+	local timer = 0
+	frame:SetScript("OnUpdate", function(self, elapsed)
+		timer = timer + elapsed
+		if timer < UPDATE_INTERVAL then
+			return
 		end
-		NextUpdate = DevilMayCry:SetSize()
-	end
+		timer = 0
+		local size = BASE_SIZE
+		percentCompleted = percentCompleted - 0.0015
+		if percentCompleted < 0 then
+			-- Test loop
+			percentCompleted = 1
+			currentScale = MAX_SCALE
+		end
+		if currentScale > 1 then
+			currentScale = currentScale - 0.06
+			if currentScale < 1 then
+				currentScale = 1
+			end
+			size = size * currentScale
+			SetSize(self, size, size)
+		end
+		SetSize(fgTexture, size * WIDTH_SCALE, size * percentCompleted)
+		if WIDTH_SCALE > 1 then
+			WIDTH_SCALE = WIDTH_SCALE - 0.005
+			if WIDTH_SCALE < 1 then
+				WIDTH_SCALE = 1
+			end
+		end
+		if currentScale == 1 then
+			SetSize(self, size, size)
+		end
+		SetTexCoord(fgTexture, 0, 1, 1 - percentCompleted, 1)
+	end)
 end
-
-UpdateFrame:SetScript("OnUpdate", DevilMayCry.OnUpdate)
